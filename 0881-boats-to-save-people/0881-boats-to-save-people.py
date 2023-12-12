6da91f2a08e8d74b6199ede8028ddc9925d81fb5
BABU <108095881+akalewoled@users.noreply.github.com>
2023-12-12 22:14:48 +0200

    Time: 384 ms (78.75%), Space: 23.1 MB (75.21%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -4,14 +4,12 @@ class Solution:
         i,j =0,len(people)-1
         final=0
         while i<=j:
-            sum=people[i]+people[j]
-            if sum<=limit:
-                final+=1
+            if people[i]+people[j]<=limit:
                 i+=1
                 j-=1
             else:
-                final+=1
                 j-=1
+            final+=1
         return final
 
         
