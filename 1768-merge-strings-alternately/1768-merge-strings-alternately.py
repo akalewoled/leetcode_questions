329bd46242a14ddf2523b127d42fc4c1b5b1abb2
BABU <108095881+akalewoled@users.noreply.github.com>
2024-02-04 12:51:31 +0300

    Time: 22 ms (99.62%), Space: 16.8 MB (55.11%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -12,5 +12,11 @@ class Solution:
                 final.append(word2[i])
             final="".join(final)+"".join(word1[i+1:]) if i<len(word1)-1 else "".join(final)
         return final
-            
+        
+        """
+        1st we compare the  the numbers so that we can use the smaller length to travesre 
+        2nd we store the chatcters alternativly startting form the first one
+        3rd we add the left out charcter of the bigger string 
+        
+        """
       
