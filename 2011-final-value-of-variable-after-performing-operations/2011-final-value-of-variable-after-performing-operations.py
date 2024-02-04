21ae40d2cbc4feefa0e25a3dffe7319346c04113
BABU <108095881+akalewoled@users.noreply.github.com>
2024-02-04 13:23:54 +0300

    Time: 51 ms (82.97%), Space: 16.7 MB (51.13%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -5,3 +5,7 @@ class Solution:
         for item in operations:
             final=final + 1 if item[1]=="+" else final - 1
         return final
+    """
+    for every elemnt in  operations i.e "x++","++x","--x","x--" pick the middle charcter 
+    add one to the final number if it is + or minus 1 if it is minus
+    """
