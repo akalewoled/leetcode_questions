6b6fd40b048d92352f28d6f93b4fceb88130952e
BABU <108095881+akalewoled@users.noreply.github.com>
2024-02-04 13:08:20 +0300

    Time: 43 ms (42.56%), Space: 16.6 MB (73.51%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -6,4 +6,8 @@ class Solution:
         for i in t:
             if i not in s or t[i]>s[i]:
                 return i
-        
+        """
+        1st we import Counter from collection  to count the reputition
+        2nd iterate through the counter to get the diffrentce
+        3rd if a number is not in the first word or its occurance is greter in the second number return it
+        """
