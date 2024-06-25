7da32e4d3de21cad8c1eb6ff621440c957f29954
fikiremariyam <108095881+akalewoled@users.noreply.github.com>
2024-06-25 10:13:02 +0300

    Time: 92 ms (43.15%), Space: 18.3 MB (69.13%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canFinish(self, n: int, edges: List[List[int]]) -> bool:
+        
+        graph={ i:[] for i in range(n)}
+        for course,pre in edges:
+            graph[pre].append(course)
+        visited = [0 for _ in range(n)]
+        pvisited=[0 for _ in range(n)]
+
+        #lets run bfs and check if we can complete all the nodes
+        def  checkcircle(node: int) ->bool:
+            
+            visited[node]=1
+            pvisited[node]=1
+
+            for item in graph[node]:
+                if visited[item]==0:
+                    if checkcircle(item) == True:
+                        return True
+                elif (pvisited[item]==1):
+                    return True
+
+            pvisited[node]=0
+            return  False
+            
+            
+        for item in graph:
+            if  visited[item] ==0 :
+                if checkcircle(item):#if there is a loop
+                    return False
+        return True # if there doesnt exist a loop nomatter the entry 
+            
+                
+            
+            
