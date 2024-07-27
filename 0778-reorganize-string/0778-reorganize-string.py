09805bb28f4f8b4bdc7e7ac82ba69630e06d9257
fikiremariyam <108095881+akalewoled@users.noreply.github.com>
2024-07-27 09:39:14 +0300

    Time: 40 ms (35.77%), Space: 16.6 MB (54.72%) - LeetHub

---
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -0,0 +1,27 @@
+from collections import Counter
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        freq=Counter(s)
+
+        maxi= max(freq.values())
+        if len(s) -maxi+1<maxi:
+            return ""
+        q=[]
+        final=[]
+        for i in freq:
+            heappush(q,(-freq[i],i))
+        
+        while len(q)>1:
+            X=heappop(q)
+            Y=heappop(q)
+            final.extend([X[1],Y[1]])
+            if X[0]<-1:
+                heappush(q,(X[0]+1,X[1]))
+            if Y[0]<-1:
+                heappush(q,(Y[0]+1,Y[1]))
+        if q:
+            if q[0][0]<-1:
+                return ""
+            final.append(q[0][1])
+        return "".join(map(str,final))
+
