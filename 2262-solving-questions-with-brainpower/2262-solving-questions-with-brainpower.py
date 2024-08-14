88bb3abf7288023dca89366095771806ac9e1b07
fikiremariyam <108095881+akalewoled@users.noreply.github.com>
2024-08-14 07:24:02 +0300

    Time: 1266 ms (61.55%), Space: 62.8 MB (62.89%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n=len(questions)
+        check=[0] * (n+1)
+        for i in range(n-1,-1,-1):
+            include=questions[i][0]+(check[i+ questions[i][1]+1] if i+questions[i][1]+1 <n else 0)
+            exclude=check[i+1]
+            check[i]=max(include,exclude)
+        return check[0]
