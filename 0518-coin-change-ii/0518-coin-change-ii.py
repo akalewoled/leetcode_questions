ef2f6df6465e2de8e1f90c46133a2b165f41c8b3
fikiremariyam <108095881+akalewoled@users.noreply.github.com>
2024-08-14 06:05:18 +0300

    Time: 4407 ms (5.01%), Space: 57.7 MB (24.26%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,16 +1,19 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n=len(coins)
-        @cache
+        check={}
         def dfs(i,summ):
            
             if summ==amount:
                 return 1
             if summ > amount :
                 return 0
+            if (summ,i) in check:
+                return check[(summ,i)]
             
             cur=0
             for j in range(i,n):
                 cur += dfs(j,summ+coins[j])
+            check[(summ,i)]=cur
             return cur
         return dfs(0,0)
