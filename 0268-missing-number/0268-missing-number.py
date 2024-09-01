f0c7f8632a7a42883ea8a2af0c2e966067e894c8
fikiremariyam <108095881+akalewoled@users.noreply.github.com>
2024-09-01 23:14:32 +0300

    Time: 90 ms (82.33%), Space: 13 MB (38.61%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,10 @@
 class Solution(object):
     def missingNumber(self, nums):
-        xor=0 
-        for  i in range(len(nums)):
-            xor=xor^ i^ nums[i]
-        return xor ^ len(nums)
+        lenth = len(nums)
+        # Calculate the sum of the first N natural numbers as (1 + lenth) * lenth/2...
+        sum = (1 + lenth) * lenth/2
+        # Traverse the array from start to end...
+        for i in nums:
+            # Find the sum of all the array elements...
+            sum -= i
+        return sum
