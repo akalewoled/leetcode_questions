c82a047e544b34e111b8976b688c450515146f32
fikiremariyam <108095881+akalewoled@users.noreply.github.com>
2024-09-05 12:03:08 +0300

    Time: 82 ms (27.43%), Space: 18.6 MB (46.51%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,3 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return ((3*sum((set(nums))))-sum(nums))//2
+        final=0
+        for i in range(32):
+            tempo=0
+            for num in nums:
+                if num < 0:
+                    num = num & (2**32-1)
+                tempo += num>> i & 1
+            tempo = tempo % 3 
+            final |=  tempo << i
+        
+        if final >= 2**31:
+            final -= 2**32
+        return final
+        
